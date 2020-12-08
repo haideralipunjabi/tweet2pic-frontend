753132761f5e09503d588b7ba32eabfd92d8234c
Haider Ali Punjabi <haiderali@cyberservices.com>
2020-12-08 10:53:10 +0530

    added analytics

--- pages/_document.js
+++ pages/_document.js
@@ -7,6 +7,19 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
+          <script dangerouslySetInnerHTML={{
+            __html=`
+            <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-YRVSTDNJYK"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-YRVSTDNJYK');
+</script>
+`
+          }}/>
         </Head>
         <body>
           <Main />
