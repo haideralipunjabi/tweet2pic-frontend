7ab04ef250a3d1db2bb03e1fa2501adcbb4067db
Haider Ali Punjabi <haiderali@cyberservices.com>
2021-04-10 22:11:31 +0530

    Added Google Site Verification

--- pages/_document.js
+++ pages/_document.js
@@ -7,6 +7,7 @@ class MyDocument extends Document {
         <Head>
         <script data-goatcounter="https://tweet2pic.goatcounter.com/count"
         async src="//gc.zgo.at/count.js"></script>
+        <meta name="google-site-verification" content="-CZnyT9ZXXRn8ojKcnr-R47fjCVKsqAnxD7fh6eKagE" />
         </Head>
         <body>
           <Main />
