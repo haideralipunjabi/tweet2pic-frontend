1c07dc8cac77273e909809715fde979fd576034b
Haider Ali Punjabi <haiderali@cyberservices.com>
2021-04-12 16:36:09 +0530

    Removed BuyMeACoffee in TWA

--- components/footer.js
+++ components/footer.js
@@ -1,6 +1,7 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
+import { useRouter } from "next/router";
 export default function Navbar() {
+  const router = useRouter();
   return (
     <footer className="footer">
       <div className="content has-text-centered">
@@ -19,7 +20,7 @@ export default function Navbar() {
           >
             Haider Ali Punjabi
           </a>{" "}
-          | <a href="https://www.buymeacoffee.com/HAliPunjabi" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon
+          | <a className={router.query.utm_source === "twa" ? "is-hidden": ""} href="https://www.buymeacoffee.com/HAliPunjabi" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon
             className="mx-2"
             icon={["fas","mug-hot"]}/>Buy Me a Coffee
           </a> | © {new Date().getFullYear()} <strong>Tweet2Pic</strong>
