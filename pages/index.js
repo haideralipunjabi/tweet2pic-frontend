cded52ca96fe217c964af5b3b73c4b31eb9ce86f
Haider Ali Punjabi <haiderali@cyberservices.com>
2021-04-14 22:09:16 +0530

    Down

--- pages/index.js
+++ pages/index.js
@@ -10,8 +10,11 @@ export default function Home() {
     >
       <Navbar></Navbar>
       <section className="section">
-        <div className="container">
-          <Interface></Interface>
+        <div className="has-text-centered container">
+          {/* <Interface></Interface> */}
+          <h1 className="title">Web App is Down</h1>
+          <br/>
+          <h2 className="subtitle">Due to excessive traffic, our Heroku backend can't handle requests. We are looking into it and will post an update soon</h2>
         </div>
       </section>
       <Footer></Footer>
